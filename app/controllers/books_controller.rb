9f74fc7357eed2ee20941f85f9f25b270cdcd216
EC2 Default User <ec2-user@ip-172-31-91-207.ec2.internal>
2022-04-30 15:56:28 +0000

    220430

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,13 +3,24 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
-  def create
-  end
-
   def index
+    @books = Book.all
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path
+    else
+      render :new
+    end
   end
 
   def update
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,39 @@
 class UsersController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path
+  def show
   end
 
   def index
-    @user = User.name
+    @user = User.find(params[:id])
+    @books = Book.all
   end
 
   def edit
     @user = User.find(params[:id])
   end
 
-  def show
+  def create
+    user = User.new(user_params)
+    user.save
+    redirect_to '/users'
+  end
+
+  def index
+    @user = User
+    @books = Book.all
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
+
 
   private
 
-  def book_params
-    params.require(:book).permit(:name,:body)
+  def user_params
+    params.require(:user).permit(:name,:profile_image,:introduction)
   end
 
 end
